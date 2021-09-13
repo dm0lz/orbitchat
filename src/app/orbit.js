2f741c6983772282ba465f6d95d46af335433df4
dm0lz <ducrouxolivier@gmail.com>
2021-09-13 17:33:36 +0200

    config

--- src/app/orbit.js
+++ src/app/orbit.js
@@ -28,8 +28,9 @@ export const orbitInstance = async () => {
             // "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
             // "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
             // "/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/",
+            process.env.REACT_APP_SIGNALLING,
             // Use local signal server
-            "/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star",
+            // "/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star",
           ],
         },
       },
